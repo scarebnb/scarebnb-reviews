e7a1e6cc00e98d8e4e7174ffdb05c925a351b8d8
Javier Zarate <javalzar17@gmail.com>
2021-01-25 10:43:01 -0800

    Ready for proxy.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const controllers = require('./controllers');
 
 const app = express();
 
 app.get('/reviews/:id', controllers.reviews.get);
 
+app.use(cors({
+  origin: 'http://localhost:8000',
+}));
+// app.use('/bundle', express.static(path.join(__dirname, '/../client/public/bundle.js')));
 app.use(express.static(path.join(__dirname, '/../client/public')));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
